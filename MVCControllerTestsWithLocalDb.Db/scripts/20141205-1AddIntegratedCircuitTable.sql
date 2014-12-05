9f16c9666366c3c0ab4bac7c08e5cfc94c6a8985
matt kocaj <mkocaj@hotmail.com>
2014-12-05 10:16:56 +0800

    added first db script to create test table

---
+++ MVCControllerTestsWithLocalDb.Db/scripts/20141205-1AddIntegratedCircuitTable.sql
@@ -0,0 +1,7 @@
+﻿create table IntegratedCircuit (
+	Id int not null identity (1,1),
+	Code nvarchar(max) not null,
+	Description nvarchar(max) not null
+)
+alter table IntegratedCircuit
+	add constraint PK_IntegratedCircuit primary key clustered ( Id ) 
