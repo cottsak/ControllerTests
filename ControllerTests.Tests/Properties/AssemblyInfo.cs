581ac4983090f7ae57674c444d8d8486567e2465
matt kocaj <mkocaj@hotmail.com>
2015-02-04 11:09:36 +0800

    adding missing name changes

--- ControllerTests.Tests/Properties/AssemblyInfo.cs
+++ ControllerTests.Tests/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MVCControllerTestsWithLocalDb.Tests")]
+[assembly: AssemblyTitle("ControllerTests.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MVCControllerTestsWithLocalDb.Tests")]
+[assembly: AssemblyProduct("ControllerTests.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
