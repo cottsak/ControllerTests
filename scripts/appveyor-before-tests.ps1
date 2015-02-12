d17aafeb8eef717da9e1fab8900cb17c2cc9613c
matt kocaj <mkocaj@hotmail.com>
2015-02-12 11:39:40 +0800

    fixing path to config file that the test runner will use

--- scripts/appveyor-before-tests.ps1
+++ scripts/appveyor-before-tests.ps1
@@ -1,4 +1,4 @@
-$config = "C:\projects\controllertests\ControllerTests.Tests\App.config"
+$config = "C:\projects\controllertests\ControllerTests.Tests\bin\Debug\ControllerTests.Tests.dll.config"
 $doc = (gc $config) -as [xml]
 $doc.SelectSingleNode('//connectionStrings/add[@name="store"]').connectionString = 'Server=.\sqlexpress; Database=ControllerTests.Web; Trusted_connection=true'
 $doc.Save($config)
