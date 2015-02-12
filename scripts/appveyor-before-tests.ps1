adadcb1de471a194ebee5bf701af928c51fce1b6
matt kocaj <mkocaj@hotmail.com>
2015-02-12 17:38:59 +0800

    updating script to include db attach

--- scripts/appveyor-before-tests.ps1
+++ scripts/appveyor-before-tests.ps1
@@ -1,4 +1,11 @@
-$config = "C:\projects\controllertests\ControllerTests.Tests\bin\Debug\ControllerTests.Tests.dll.config"
+# replace the db connection with the local instance 
+$startPath = "C:\projects\controllertests\ControllerTests.Tests\bin\Debug\"
+$config = join-path $startPath "ControllerTests.Tests.dll.config"
 $doc = (gc $config) -as [xml]
 $doc.SelectSingleNode('//connectionStrings/add[@name="store"]').connectionString = 'Server=.\sqlexpress; Database=ControllerTests.Web; Trusted_connection=true'
 $doc.Save($config)
+
+# attach mdf to local instance
+$mdfFile = join-path $startPath "store.mdf"
+$ldfFile = join-path $startPath "store_log.ldf"
+sqlcmd -S .\SQL2014 -U sa -P Password12! -Q "sp_attach_db 'ControllerTests.Web', '$mdfFile', '$ldfFile'"
