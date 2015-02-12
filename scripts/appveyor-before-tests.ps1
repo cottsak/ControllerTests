8cdfc9a82674dd019d77c1f547861c3cea5245c6
matt kocaj <mkocaj@hotmail.com>
2015-02-12 11:29:55 +0800

    fixing xpath bug in script

--- scripts/appveyor-before-tests.ps1
+++ scripts/appveyor-before-tests.ps1
@@ -1,4 +1,4 @@
 $config = "C:\projects\controllertests\ControllerTests.Tests\App.config"
 $doc = (gc $config) -as [xml]
-$doc.SelectSingleNode('//connectionStrings/add[@name="store"]/@connectionstring').'#text' = 'Server=.\sqlexpress; Database=ControllerTests.Web; Trusted_connection=true'
+$doc.SelectSingleNode('//connectionStrings/add[@name="store"]').connectionString = 'Server=.\sqlexpress; Database=ControllerTests.Web; Trusted_connection=true'
 $doc.Save($config)
