1d83272e3f76c615f9dcfa3cd23d0813f07e38c0
matt kocaj <mkocaj@hotmail.com>
2015-02-13 09:10:25 +0800

    clean up

--- scripts/appveyor-before-tests.ps1
+++ scripts/appveyor-before-tests.ps1
@@ -3,7 +3,6 @@ $sqlInstance = "(local)\SQL2012SP1"
 $dbName = "ControllerTests.Web"
 
 # replace the db connection with the local instance 
-
 $config = join-path $startPath "ControllerTests.Tests.dll.config"
 $doc = (gc $config) -as [xml]
 $doc.SelectSingleNode('//connectionStrings/add[@name="store"]').connectionString = "Server=$sqlInstance; Database=$dbName; Trusted_connection=true"
@@ -12,5 +11,4 @@ $doc.Save($config)
 # attach mdf to local instance
 $mdfFile = join-path $startPath "store.mdf"
 $ldfFile = join-path $startPath "store_log.ldf"
-#sqlcmd -S "$sqlInstance" -U sa -P Password12! -Q "sp_attach_db '$dbName', '$mdfFile', '$ldfFile'"
 sqlcmd -S "$sqlInstance" -Q "Use [master]; CREATE DATABASE [$dbName] ON (FILENAME = '$mdfFile'),(FILENAME = '$ldfFile') for ATTACH"
