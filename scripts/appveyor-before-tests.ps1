f921a25e43ad39c22b9820b72c52bdf134b339d1
matt kocaj <mkocaj@hotmail.com>
2015-02-13 08:40:21 +0800

    fixing connection string eval. extracting some vars.

--- scripts/appveyor-before-tests.ps1
+++ scripts/appveyor-before-tests.ps1
@@ -1,12 +1,16 @@
-# replace the db connection with the local instance 
 $startPath = "C:\projects\controllertests\ControllerTests.Tests\bin\Debug\"
+$sqlInstance = "(local)\SQL2012SP1"
+$dbName = "ControllerTests.Web"
+
+# replace the db connection with the local instance 
+
 $config = join-path $startPath "ControllerTests.Tests.dll.config"
 $doc = (gc $config) -as [xml]
-$doc.SelectSingleNode('//connectionStrings/add[@name="store"]').connectionString = 'Server=.\sqlexpress; Database=ControllerTests.Web; Trusted_connection=true'
+$doc.SelectSingleNode('//connectionStrings/add[@name="store"]').connectionString = "Server=$sqlInstance; Database=$dbName; Trusted_connection=true"
 $doc.Save($config)
 
 # attach mdf to local instance
 $mdfFile = join-path $startPath "store.mdf"
 $ldfFile = join-path $startPath "store_log.ldf"
-#sqlcmd -S .\SQL2014 -U sa -P Password12! -Q "sp_attach_db 'ControllerTests.Web', '$mdfFile', '$ldfFile'"
-sqlcmd -S "(local)\SQL2012SP1" -Q "Use [master]; CREATE DATABASE [ControllerTests.Web] ON (FILENAME = '$mdfFile'),(FILENAME = '$ldfFile') for ATTACH"
+#sqlcmd -S "$sqlInstance" -U sa -P Password12! -Q "sp_attach_db '$dbName', '$mdfFile', '$ldfFile'"
+sqlcmd -S "$sqlInstance" -Q "Use [master]; CREATE DATABASE [$dbName] ON (FILENAME = '$mdfFile'),(FILENAME = '$ldfFile') for ATTACH"
