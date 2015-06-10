2464d0164397875aa975751ed4c9af0ddfed2e78
matt kocaj <mkocaj@hotmail.com>
2015-06-10 16:02:46 +0800

    copy update

--- ControllerTests/Constants.cs
+++ ControllerTests/Constants.cs
@@ -5,6 +5,6 @@ namespace ControllerTests
     static class Constants
     {
         public static readonly Func<Exception> BadBuilderConfigOrderException = () => 
-            new InvalidOperationException("Can not configure after the server has been initialised. Always ConfigureServices before using the Session member. This exception also may indicate that your tests are running in parallel and are not thread safe.");
+            new InvalidOperationException("Can not configure after the server has been initialised. Always ConfigureServices/SubstituteAndConfigure before using the Session member. This exception also may indicate that your tests are running in parallel and are not thread safe.");
     }
 }
