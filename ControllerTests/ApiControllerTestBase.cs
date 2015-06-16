f239bafbb3963304fc3a81bde8067aec19c2ba9e
matt kocaj <mkocaj@hotmail.com>
2015-06-16 13:59:37 +0800

    attempting to get thread safety for NCrunch

--- ControllerTests/ApiControllerTestBase.cs
+++ ControllerTests/ApiControllerTestBase.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Web.Http;
 using Autofac;
 using Autofac.Integration.WebApi;
@@ -57,7 +57,7 @@ namespace ControllerTests
                 }
 
                 return new HttpServer(config);
-            });
+            }, LazyThreadSafetyMode.ExecutionAndPublication);
         }
 
         protected void ConfigureServices(Action<ContainerBuilder> config)
